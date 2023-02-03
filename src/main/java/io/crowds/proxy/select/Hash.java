a566ba3ad02dcc59c5f21abec26aa92ecf95e9c7
Storm <stormpxx@gmail.com>
2023-02-03 17:16:55 +0800

    impl hash selector correctly

--- src/main/java/io/crowds/proxy/select/Hash.java
+++ src/main/java/io/crowds/proxy/select/Hash.java
@@ -2,38 +2,42 @@ package io.crowds.proxy.select;
 
 import io.crowds.proxy.NetAddr;
 import io.crowds.proxy.ProxyContext;
+import io.crowds.util.MurmurHash;
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Hash extends TransportSelector{
 //    private Map<String,String> tagMap;
     private Map<String,String> virtualMap;
-    private final TreeMap<Integer,String> hashRing;
+    private final TreeMap<Long,String> hashRing;
 
     public Hash(String name,Map<String,String> virtualMap,List<String> tags) {
         super(name);
         Objects.requireNonNull(tags);
-        assert !tags.isEmpty();
 //        this.tagMap= Collections.emptyMap();
-        this.virtualMap=virtualMap==null?new HashMap<>():virtualMap;
+        this.virtualMap=virtualMap==null?Map.of():virtualMap;
         this.hashRing =new TreeMap<>();
-        tags.forEach(str->this.hashRing.put(hash(str),str));
+        tags.forEach(str->this.hashRing.put(hash(str.getBytes(StandardCharsets.US_ASCII)),str));
+        this.virtualMap.keySet().forEach(str->this.hashRing.put(hash(str.getBytes(StandardCharsets.US_ASCII)),str));
     }
 
-    private int hash(Object obj){
-        return Math.abs(Objects.hashCode(obj));
+    private long hash(byte[] content){
+        return Integer.toUnsignedLong(MurmurHash.hash32(content,content.length));
     }
 
 
     @Override
     public List<String> tags() {
-        return new ArrayList<>(this.hashRing.values());
+        return this.hashRing.values().stream().filter(Predicate.not(virtualMap::containsKey)).toList();
     }
 
     @Override
     public String nextTag(ProxyContext proxyContext) {
         NetAddr netAddr = proxyContext.getNetLocation().getSrc();
-        int hash = Objects.hash(netAddr.getAsInetAddr().getAddress());
+        byte[] srcIpBytes = netAddr.getAsInetAddr().getAddress().getAddress();
+        long hash = hash(srcIpBytes);
 
         var entry = this.hashRing.ceilingEntry(hash);
         if (entry==null){
